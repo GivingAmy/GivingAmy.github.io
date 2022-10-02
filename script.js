2fe200f8a2ff44e53cba6aa729acc682ec30191e
Ewynman <edward.wynman@gmail.com>
2022-10-02 00:11:05 -0400

    10/2/2022

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.getElementById("current_date").innerHTML =
   month + "/" + day + "/" + year;
 
 //Countdown JS
-var countDownDate = new Date("Sep 30, 2022 08:37:25").getTime();
+var countDownDate = new Date("Oct 6, 2022 18:30:00").getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
